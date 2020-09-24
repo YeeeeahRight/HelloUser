e9a7995082f04cac2ca83c03cf4c2cb076490e63
YeeeeahRight <yeeeeeah.right@gmail.com>
2020-09-24 13:20:31 +0300

    Deleted console input and added new tests.

--- src/main/java/com/epam/hellouser/data/DataAcquirerFactory.java
+++ src/main/java/com/epam/hellouser/data/DataAcquirerFactory.java
@@ -8,11 +8,9 @@ public class DataAcquirerFactory {
     private static final String UNKNOWN_INPUT_WAY_MESSAGE = "Unknown input way.";
 
     public DataAcquirer createDataAcquirer(InputWayEnum way) throws UnknownInputWayException {
-        switch (way) {
-            case ARGUMENTS:
-                return new ArgumentsDataAcquirer();
-            default:
-                throw new UnknownInputWayException(UNKNOWN_INPUT_WAY_MESSAGE);
+        if (way == InputWayEnum.ARGUMENTS) {
+            return new ArgumentsDataAcquirer();
         }
+        throw new UnknownInputWayException(UNKNOWN_INPUT_WAY_MESSAGE);
     }
 }
